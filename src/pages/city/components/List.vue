b0fcaf92ad6f1422e4b9703c3138f911d02ea241
SHorion <42463247+SHorion@users.noreply.github.com>
2018-12-08 20:50:08 +0800

    use vuex

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,45 +1,75 @@
 <template>
-	<div class="list" ref="wrapper">
-		<div>
-	      <div class="area">
-	        <div class="title border-topbottom">当前城市</div>
-	        <div class="button-list">
-	          <div class="button-wrapper">
-	            <div class="button">北京</div>
-	          </div>
-	        </div>
-	      </div>
-	      <div class="area">
-	        <div class="title border-topbottom">热门城市</div>
-	        <div class="button-list">
-	          <div class="button-wrapper" v-for="item of hot" :key="item.id">
-	            <div class="button">{{item.name}}</div>
-	          </div>
-	        </div>
-	      </div>
-	      <div 
-          class="area" 
-          v-for="(item, key) of cities" 
-          :key="key"
-          :ref="key">
-	        <div class="title border-topbottom">{{key}}</div>
-	        <div class="item-list" v-for="innerItem of item" :key="innerItem.id">
-	          <div class="item border-bottom">{{innerItem.name}}</div>
-	        </div>
-	      </div>
-	     </div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">{{this.currentCity}}
+            <!-- <div class="button">{{this.$store.state.city}} -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id" 
+            @click="handleCityClick(item.name)"
+          >
+            <div class="button">{{item.name}}</div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+      >
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id" 
+            @click="handleCityClick(innerItem.name)"
+          >
+            {{innerItem.name}}
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+
 
 <script>
 import BScroll from 'better-scroll'
+import { mapState } from 'vuex'
 export default {
 	name: 'CityList',
+  computed: {
+    ...mapState({
+      currentCity: 'city'
+    })
+  },
 	props: {
 		hot: Array,
 		cities: Object,
     letter: String
 	},
+  methods: {
+    handleCityClick (city) {
+      this.$store.commit('changeCity', city)
+      // this.$store.dispatch('changeCity', city)
+      // alert(city)
+      this.$router.push('/')
+    },
+  },
 	mounted () {
 		this.scroll = new BScroll(this.$refs.wrapper)
 	},
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,12 +3,21 @@
     <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
-    <div class="search-content" ref="search" v-show="keyword">
+    <div
+      class="search-content"
+      ref="search"
+      v-show="keyword"
+    >
       <ul>
-        <li class="search-item border-bottom" v-for="item of list">
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          @click="handleCityClick(item.name)"
+        >
           {{item.name}}
         </li>
-        <li class="search-item border-bottom" v-show="hasNoData" :key="item.id">
+        <li class="search-item border-bottom" v-show="hasNoData">
           没有找到匹配数据
         </li>
       </ul>
@@ -17,49 +26,58 @@
 </template>
 
 <script>
-import BScroll from 'better-scroll'
+import Bscroll from 'better-scroll'
+import { mapMutations } from 'vuex'
 export default {
-	name: 'CitySearch',
-	props: {
-		cities: Object
-	},
-	data () {
-		return {
-			keyword: '',
-			list: [],
-			timer: null
-		}
-	},
-	computed: {
-		hasNoData () {
-			return !this.list.length
-		}
-	},
-	watch: {
-		keyword () {
-			if (this.timer) {
-				clearTimeout(this.timer)
-			}
-			if (!this.keyword) {
-				this.list = []
-				return 
-			}
-			this.timer = setTimeout(() => {
-				const result = []
-				for (let i in this.cities) {
-					this.cities[i].forEach((value) => {
-						if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
-							result.push(value)
-						}
-					})
-				}
-				this.list = result
-			}, 100)
-		}
-	},
-	mounted () {
-		this.scroll = new BScroll(this.$refs.search)
-	}
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.$store.commit('changeCity', city)
+      // this.$store.dispatch('changeCity', city)
+      // alert(city)
+      this.$router.push('/')
+    },
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
